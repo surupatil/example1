df22d30f3f7713a044d7ebd9b5b7cf29156ca6c5
surupatil <surabhi.patil237@gmail.com>
2022-03-24 14:40:33 +0530

    ex2

---
+++ javapro/src/Loops/While_100_1_print.java
@@ -0,0 +1,16 @@
+package Loops;
+
+public class While_100_1_print {
+
+	public static void main(String[] args)
+	{
+		int i = 1;
+		while (i<=100)
+		{
+
+			System.out.print(i + " ");
+
+			i++;
+		}
+	}
+}
