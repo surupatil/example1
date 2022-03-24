ed24b0cbea4dce3b289eb134c86e0944dd0e11ae
surupatil <surabhi.patil237@gmail.com>
2022-03-24 12:44:23 +0530

    comit1

---
+++ javapro/src/Control_Conditional_Statment/ELSE_IF.java
@@ -0,0 +1,16 @@
+package Control_Conditional_Statment;
+
+public class ELSE_IF {
+	public static void main(String[]args){
+		int marks=68;
+		if(marks>65){
+			System.out.println("Result:Firstclass");
+		}else if(marks>50){
+				System.out.println("Result:Secondclass");
+			}else if(marks>=35){
+				System.out.println("Result:pass");
+			}else{
+					System.out.println("Result:fail");
+				}
+			}
+}
